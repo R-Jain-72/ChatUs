b22fe9cf7fd26ead9a9ecf9427e8cd795428101e
Rishabh J <rishabhj1470@gmail.com>
2024-05-06 23:51:34 +0530

    Login Page Done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,12 +9,26 @@ const Login = lazy(() => import('./pages/Login'));
 const Chat = lazy(() => import('./pages/Chat'));
 const Groups = lazy(() => import('./pages/Groups'));
 
+//let user = true;
+let user = false;
+
 const App = () => {
   return (
     <BrowserRouter>
       {/* <span> Header(this will be common for all routes) </span> */}
       <Routes>
+        {/*
         <Route path="/" element={<Home />} />  {/* Use the imported component */}
+        
+        {/* if the user in not logged in the home page will redirect it back to the login page*/}
+        <Route 
+          path="/" 
+          element={
+            <ProtectRoute user={user}>
+              <Home />
+            </ProtectRoute>
+          }
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/chat/:chatId" element={<Chat />} /> {/* used for dynamic routing */}
         <Route path="/groups" element={<Groups />} />
