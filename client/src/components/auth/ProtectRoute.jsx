c8bda549e80eff0a132b66022038d24679ba8af8
Rishabh J <rishabhj1470@gmail.com>
2024-05-14 22:37:03 +0530

    Login Page Done

--- client/src/components/auth/ProtectRoute.jsx
+++ client/src/components/auth/ProtectRoute.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
-const ProtectRoute = ({children,user,redirect="/login"}) => {
-    if(!user) return <Navigate to={redirect} />;
-    return children;
-};
+
 
 export default ProtectRoute;
